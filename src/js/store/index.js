02e48e661f42c9671a304fb4f060e4503b1beb56
Tomas <tomasfreire@gmail.com>
2019-09-09 19:21:48 -0500

    first commit

---
+++ src/js/store/index.js
@@ -0,0 +1,13 @@
+import { createStore, applyMiddleware, compose } from "redux";
+import rootReducer from "../reducers/index";
+import { forbiddenWordsMiddleware } from "../middleware/index";
+import thunk from "redux-thunk";
+
+const storeEnhancers = window._REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+  rootReducer,
+  storeEnhancers(applyMiddleware(forbiddenWordsMiddleware, thunk))
+);
+
+export default store;
